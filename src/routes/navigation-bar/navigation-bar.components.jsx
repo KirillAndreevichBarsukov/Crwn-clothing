05066fb94ac5f53b7dbaba65ca7896daee11df1f
KirillAndreevichBarsukov <barsukov.kirill.a@gmail.com>
2024-06-17 22:05:26 +0300

    add navigation-bar with logo.svg, add routes '/' and '/shop' ,delete package-lock.json

---
+++ src/routes/navigation-bar/navigation-bar.components.jsx
@@ -0,0 +1,26 @@
+import { Outlet, Link } from 'react-router-dom';
+import { Fragment } from 'react';
+
+import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
+
+import './navigation-bar.styles.scss';
+
+const NavigationBar = () => {
+  return (
+    <Fragment>
+      <div className='navigation-bar'>
+        <Link className='logo-container' to='/'>
+          <CrwnLogo />
+        </Link>
+        <div className='nav-links-container'>
+          <Link className='nav-bar-link' to='shop'>
+            Shop
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </Fragment>
+  );
+};
+
+export default NavigationBar;
